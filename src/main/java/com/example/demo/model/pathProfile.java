350b376c58c8dc49f83edb9e065024ce989455b5
cocoiscat <demy176@naver.com>
2022-09-17 01:10:53 +0900

    feat: create pathProfile.java

---
+++ src/main/java/com/example/demo/model/pathProfile.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class pathProfile {
+    public String departName;
+    public String destName;
+    public String hour;
+    public String minute;
+    public String week;
+}
